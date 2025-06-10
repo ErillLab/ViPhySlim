6c9a64288894e67e5e6ee9a4be173a563fdc4d15
Nerea Garrido <nerusita444@gmail.com>
2025-06-10 10:18:12 +0200

    If the user chooses bootstrap, that algorithm is executed.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from mpi4py import MPI
 import preprocessing
+import algorithm_bootstrap
 import algorithm
+import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+
+start = time.time()
 
 if rank == 0:
     preprocessing.preprocessing()
 comm.Barrier()
 
-algorithm.algorithm()
+if preprocessing.bootstrapping > 0:
+    algorithm_bootstrap.algorithm()
+else:
+    algorithm.algorithm
+
+if rank == 0:
+    end = time.time()
+    print(f"Runtime: {end - start} seconds")
